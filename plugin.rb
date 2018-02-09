77a1713a87e9e4affb648c670ea9858fa50a1af0
dangtrungluong <dangtrungluong@gmail.com>
2018-02-09 23:52:48 +0700

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-settings
+# about: Example plugin to override a site setting
+# version: 1.0.0
+# authors: dangtrungluong
+# url: https://github.com/dangtrungluong/discourse-settings
